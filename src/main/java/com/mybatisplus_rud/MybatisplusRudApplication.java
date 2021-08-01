c3601f507bdd850069e07912a8e0244d8ad76562
YAH919098892 <919098892@qq.com>
2021-08-01 21:56:05 +0800

    修改启动类

--- src/main/java/com/mybatisplus_rud/MybatisplusRudApplication.java
+++ src/main/java/com/mybatisplus_rud/MybatisplusRudApplication.java
@@ -9,6 +9,7 @@ public class MybatisplusRudApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisplusRudApplication.class, args);
         System.out.println("项目启动成功==========");
+        System.out.println("我是项目启动类");
     }
 
 }
