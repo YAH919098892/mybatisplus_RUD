9df8453d2b69998e21a5927f06be55d397ae6a95
YAH919098892 <919098892@qq.com>
2021-08-02 14:28:06 +0800

    111

--- src/main/java/com/mybatisplus_rud/MybatisplusRudApplication.java
+++ src/main/java/com/mybatisplus_rud/MybatisplusRudApplication.java
@@ -7,9 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MybatisplusRudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybatisplusRudApplication.class, args);
         System.out.println("项目启动成功==========");
         System.out.println("我是项目启动类");
+        SpringApplication.run(MybatisplusRudApplication.class, args);
+
         System.out.println("我是杨安华");
     }
 
