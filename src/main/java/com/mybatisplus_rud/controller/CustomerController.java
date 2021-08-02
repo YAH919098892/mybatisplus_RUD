c1caa4c9b74afcacd0d79aecc52081ccddcd0d2e
YAH919098892 <919098892@qq.com>
2021-08-02 15:12:54 +0800

    3333

--- src/main/java/com/mybatisplus_rud/controller/CustomerController.java
+++ src/main/java/com/mybatisplus_rud/controller/CustomerController.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     public void an(){
         System.out.println("55555555555");
+        System.out.println("7777777");
     }
     public void an(String name){
         System.out.println("555555");
+        System.out.println("7777777777");
     }
     public void an2(String name){
         System.out.println("2222222");
